b44f599f81fabec1ce17a2573720106b6ed6eb52
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-13 11:24:01 +0700

    menambahkan string get judul pada kelas buku dan membuat int main, membuat objek"bukunya "serta memanggil setJudul  dan getJudul menggunakan chain function

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -4,8 +4,17 @@ class buku{
     string judul;
 
     public:
-    buku setjudul(string judul){
+    buku setJudul(string judul){
         this->judul = judul;
         return *this; // Mengembalikan objek saat ini / chain function
     }
+    string getJudul(){
+        return this->judul; // Mengembalikan nilai judul / chain function
+    }
 };
+
+int main(){
+    buku bukunya;
+    cout<<bukunya.setJudul("Matematika").getJudul();    //chain function
+    return 0;
+}
