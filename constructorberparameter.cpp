addadbd04c65c56e62cda9e108b47b0a5d746df6
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-13 10:31:45 +0700

    add main function to instantiate mahasiswa object

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -11,3 +11,7 @@ mahasiswa::mahasiswa(int nim, string nama) {
     cout << "NIM: " << nim << endl;
     cout << "Nama: " << nama << endl;
 }
+int main(){
+    mahasiswa mhs(12345, "Abra");   // memanggil constructor dengan  parameter
+    return 0;
+}
