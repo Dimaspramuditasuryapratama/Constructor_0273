eb1cf768269e821bb4572d14ea2232b12296f7a7
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-13 11:07:04 +0700

    add setget method to buku class for managing/mengelola judul attribute

--- this.cpp
+++ this.cpp
@@ -2,4 +2,9 @@
 using namespace std;
 class buku{
     string judul;
+    public:
+    string setget(string judul){
+        this->judul = judul;    //nilai parameter 'judul' untuk memberi variabel 'judul'
+        return this->judul;     //return nilai variabel judul.
+    }
 };
